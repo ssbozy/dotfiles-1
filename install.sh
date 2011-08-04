cf26292dc1262fa4723e910505bcc78333dd4e17
Pavel Lishin <pavel.lishin@gmail.com>
2011-08-04 10:06:49 -0400

    THoughts about installer

---
+++ install.sh
@@ -0,0 +1,7 @@
+# Assume that it's been cloned into ~/dotfiles/
+
+# mkdir ~/dot-bak/
+# cd dotfiles
+# find . -type f -not -ipath '*.git/*' -exec mv ~/{} ~/dot-bak/ \;
+# cd ~
+# find dotfiles/ -type f -not -ipath '*.git/*' -exec ln -s {} \;
