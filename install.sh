6e0e33ababf0a566a2cb69852278a0c74ef93bba
Pavel Lishin <pavel.lishin@gmail.com>
2011-08-09 20:45:45 -0400

    Dumb shit

--- install.sh
+++ install.sh
@@ -7,3 +7,7 @@
 # find . -type f -not -ipath '*.git/*' -exec mv ~/{} ~/dot-bak/ \;
 # cd ~
 # find dotfiles/ -type f -not -ipath '*.git/*' -exec ln -s {} \;
+
+# For our CentOS VMs, if I want tmux, I gotta:
+# rpm -ivh http://download.fedoraproject.org/pub/epel/5/i386/epel-release-5-4.noarch.rpm
+# yum install tmux
