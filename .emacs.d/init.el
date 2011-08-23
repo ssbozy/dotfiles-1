ba141b10939c87d33fb9f6a3dcdbcaf1270024b4
Pavel Lishin <pavel.lishin@gmail.com>
2011-08-23 14:22:15 -0400

    Backing stuff up to dotfiles

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -37,14 +37,14 @@
 (ido-mode t)
 (ido-everywhere t)
 
-;; I dunno, man, emacs did this shit on its own when I changed my default font shit.
+;; dear emacs, please stop shitting files everywhere
 (custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+  '(auto-save-file-name-transforms '((".*" "~/.emacs.d/autosaves/\\1" t)))
+  '(backup-directory-alist '((".*" . "~/.emacs.d/backups/"))))
+;; create the autosave dir if necessary, since emacs won't.
+(make-directory "~/.emacs.d/autosaves/" t)
 
+;; I dunno, man, emacs did this shit on its own when I changed my default font shit.
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
