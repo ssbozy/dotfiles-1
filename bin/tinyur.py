32fe52b34264d21c0cb272711816269a38b81335
Pavel Lishin <pavel.lishin@gmail.com>
2012-01-07 13:58:51 -0500

    Vastly improved tinyur uploader by using requests instead of curl, etc

--- bin/tinyur.py
+++ bin/tinyur.py
@@ -7,18 +7,30 @@ $ bin/tinyur.py
 http://i.imgur.com/NEhW5.jpg @ 2011-08-22 17:34:12 from /Users/pavel/Temp/Screen Shot 2011-08-22 at 17.29.07.png.jpg
 Unable to parse JSON response:
 Traceback (most recent call last):
-  File "bin/tinyur.py", line 109, in <module>
-    print "Unable to parse JSON response:", err.error
+File "bin/tinyur.py", line 109, in <module>
+print "Unable to parse JSON response:", err.error
 AttributeError: 'exceptions.OSError' object has no attribute 'error'
 """
 
-
 import os, re, pycurl, simplejson, pprint, subprocess, datetime, time
 
-# regex = re.compile("^Picture [\d()]+.png$")
+import sys
+import os
+import re
+import subprocess
+import datetime
+import time
+import simplejson
+
+# https://github.com/kennethreitz/requests
+import requests
+
 regex = re.compile("^Screen shot.*\.png$", re.I)
+log_file = '/Users/pavel/tinyur.log'
 searchdir = '/Users/pavel/Desktop/'
 storedir = '/Users/pavel/Temp/'
+imgur_url = 'http://imgur.com/api/upload.json'
+imgur_key = '2ed98178160882e2570722d158adad06'
 
 def getClipboardData():
   p = subprocess.Popen(['pbpaste'], stdout=subprocess.PIPE)
@@ -32,98 +44,63 @@ def setClipboardData(data):
   p.stdin.close()
   retcode = p.wait()
 
-def openClipboard():
-  pass # no-op on the mac
+def log(line, growl = True):
+  t = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-def closeClipboard():
-  pass # no-op on the mac
+  try:
+    f = open( log_file, 'a' )
+    f.write( "%s %s\n" % (t, line) )
+    f.close()
+  except IOError as err:
+    #print "Warning: unable to open log file %s" % (filepath,)
+    pass
 
-class Buffer:
-    def __init__(self):
-        self.contents = ''
+  if growl:
+    args = [ '/usr/local/bin/growlnotify',
+             '-t', 'tinyur',
+             '-m', '%s' % (line, ),
+             '--sticky' ]
+    p = subprocess.call( args = args, stdout = None, stderr = None )
 
-    def write(self, buf):
-        self.contents = self.contents + buf
+while True:
+  time.sleep(1)
 
-while 1:
-    time.sleep(1)
+  for filename in os.listdir( searchdir ):
 
-    for filename in os.listdir( searchdir ):
+    if regex.search( filename ) is None:
+      # Not a screenshot
+      continue
 
-        if not regex.search( filename ) == None:
-            
-            # move file to ~/Temp to get it out of my site
-            os.rename( searchdir + filename, storedir + filename )
+    try:
 
-            filename_jpg = filename + '.jpg'
+      # Move file off the desktop
+      os.rename( os.path.join(searchdir, filename),
+                 os.path.join(storedir, filename ))
 
-            # Let's find out if a JPEG would suit our purposes better.
-            args = [ '/usr/bin/env', 'convert', storedir + filename, storedir + filename_jpg ]
-            p = subprocess.call( args = args, stdout = None, stderr = None )
-        
-            png_size = float(os.path.getsize(storedir + filename))
-            jpg_size = float(os.path.getsize(storedir + filename_jpg))
+      filepath = os.path.join( storedir, filename )
 
-            upload_file = storedir + filename
-            if jpg_size < png_size * .5:
-                upload_file = storedir + filename_jpg
-            
-            c = pycurl.Curl()
-            values = [ 
-                ("key", "2ed98178160882e2570722d158adad06"),
-                ("image", (c.FORM_FILE, upload_file))
-            ]
+      data = { 'key' : imgur_key }
+      files = { 'image' : open( filepath, 'rb') }
 
-            b = Buffer()
+      r = requests.post( imgur_url, data=data, files=files )
 
-            c.setopt(c.URL, "http://imgur.com/api/upload.json")
-            c.setopt(c.WRITEFUNCTION, b.write)
-            c.setopt(c.HTTPPOST, values)
+      if r.status_code is not requests.codes.ok:
+        # bad HTTP response code
+        raise Exception("Invalid response from imgur: %s %s" % (r.status_code, r.content))
 
-            try:
-                c.perform()
-            except pycurl.error:
-                # I guess we couldn't connect.
-                print "CURL Error:", pycurl.error
-                continue
-        
+      j = simplejson.loads(r.content)
+      if j['rsp']['stat'] != 'ok':
+        raise Exception("Unable to upload image: %s %s" % (j['rsp']['image']['error_code'],
+                                                           j['rsp']['image']['error_msg']))
 
-            if b.contents == "":
-                print "imgur returned empty string, unable to parse."
-                continue
-
-            try:
-                j = simplejson.loads(b.contents)
-
-                if j['rsp']['stat'] == 'ok':
-
-                    # print date and time here as well
-                    t = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-                    url = j['rsp']['image']['original_image']
-                    print url, "@", t, "from", upload_file
-
-                    setClipboardData( j['rsp']['image']['original_image'] ) # copy to buffer
-
-                    # Notify us
-                    args = [ '/usr/local/bin/growlnotify', '-t', 'Screenshot Uploaded', '-m', '%s uploaded from %s' % (url, upload_file), '--sticky' ]
-                    p = subprocess.call( args = args, stdout = None, stderr = None )
-
-                    try:
-                        os.remove( storedir + filename )     # delete png file
-                        os.remove( storedir + filename_jpg ) # delete jpg file
-                    except OSError:
-                        # Maybe two copies of pfuploader running at the same time? Weird.
-                        pass
-
-            except Exception as err: # simplejson.decoder.JSONDecodeError ?
-                # I guess we couldn't connect.
-                print "Unable to parse JSON response:", err.error
-                print "JSON response: ", b.contents
-                os.remove( storedir + filename )     # delete png file
-                os.remove( storedir + filename_jpg ) # delete jpg file
-                continue
-
-    
-            c.close()
-
-
+      log("Uploaded %s" % ( j['rsp']['image']['original_image'], ))
+      log("Delete hash: %s" % ( j['rsp']['image']['delete_hash'], ), False) # no growl
+      log("Delete page: %s" % ( j['rsp']['image']['delete_page'], ), False) # no growl
+      setClipboardData( j['rsp']['image']['original_image'] )
+      os.remove( filepath )
+    except simplejson.JSONDecodeError as e:
+      log("Invalid JSON received from imgur: %s" % ( r.content, ))
+    except Exception as e:
+      log("Unable to upload screenshot: %s" % ( e, ))
+    except OSError:
+      log("Unable to move or remove file %s" % ( filepath, ))
