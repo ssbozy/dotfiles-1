eb59a39de1437692f4127fb9115dfa7ea5c289bb
Pavel Lishin <pavel.lishin@gmail.com>
2012-09-07 01:23:34 -0400

    Improved backup script

--- bin/backup.sh
+++ bin/backup.sh
@@ -61,14 +61,17 @@ dest_camera=""
 dest_itunes=""
 dest_porn=""
 dest_mirror=""
+dest_logs=""
 
 # disabling auto-deletion since I plan on nuking ~/Documents - and running backup before it's restored
 # would severely fuck me.
-AND_delete="" # change to --delete to delete stuff
+OPT_DELETE="" # change to --delete to delete stuff
+OPT_DRYRUN="" # Change to -n to only do a dry run, leave blank otherwise
 
 
 if [[ "$LOCATION" == "work" ]]; then
     hostname="moobox.netomat.net"
+    dest_logs="~/Documents/irclogs/bip-logs"
     dest_docs="/media/MooDrive/pavel/backup/index/"
     dest_projects="/media/MooDrive/pavel/backup/projects/"
     dest_images="/media/MooDrive/pavel/backup/images/"
@@ -91,6 +94,7 @@ if [[ "$LOCATION" == "work" ]]; then
     fi
 
 elif [[ "$LOCATION" == "home" ]]; then
+    dest_logs="~/Documents/irclogs/bip-logs"
     hostname="192.168.1.100"
     dest_docs="/media/asimov/index/"
     dest_projects="/media/asimov/projects/"
@@ -100,23 +104,29 @@ elif [[ "$LOCATION" == "home" ]]; then
     dest_itunes="/media/asimov/Yonkpod/YonkLibrary/"
     dest_porn="/media/niven/porn/"
     dest_mirror="/media/asimov/Downloads/mirror/"
+
 else
     print_red "Invalid location specified.";
     exit 1;
 fi
 
 # Grab the bip logs
-rsync -r --partial --progress -v --exclude='bip.log' lishin.org:~/.bip/logs ~/Documents/irclogs/bip-logs
-rsync -r --partial --progress -v --exclude='bip.log' moobox.netomat.net:~/.bip/logs ~/Documents/irclogs/bip-logs
+if [[ "$dest_logs" != "" && -d "$dest_logs" ]]; then
+    print_green "Backing up bip logs to $dest_logs"
+    rsync $OPT_DRYRUN -r --partial --progress -v --exclude='- bip.log' lishin.org:~/.bip/logs ~/Documents/irclogs/bip-logs
+    rsync $OPT_DRYRUN -r --partial --progress -v --exclude='- bip.log' moobox.netomat.net:~/.bip/logs ~/Documents/irclogs/bip-logs
+else
+    print_yellow "Not backing up bip logs to $dest_logs"
+fi
 
 # Backup documents
 if [[ "$dest_docs" != "" ]]; then
     print_green "Backing up documents to $hostname:$dest_docs"
-    rsync -a -r -z -v -u -h $AND_delete --progress --partial --timeout=30 \
+    rsync $OPT_DRYRUN -a -r -z -v -u -h $OPT_DELETE --progress --partial --timeout=30 \
         ~/Documents/  \
-        --exclude=netomat/mobilityserver \
-        --exclude=netomat/csmobility \
-        --exclude=netomat/nycgo \
+        --exclude="- netomat/mobilityserver/" \
+        --exclude="- netomat/csmobility/" \
+        --exclude="- netomat/nycgo/" \
         --exclude=*.screenflow \
         $hostname:$dest_docs
 else
@@ -126,8 +136,8 @@ fi
 # Backup projects
 if [[ "$dest_projects" != "" ]]; then
     print_green "Backing up projects to $hostname:$dest_projects"
-    rsync -a -r -z -v -u -h $AND_delete --progress --partial --timeout=30 \
-        --exclude=*.vdi \
+    rsync $OPT_DRYRUN -a -r -z -v -u -h $OPT_DELETE --progress --partial --timeout=30 \
+        --exclude="- *.vdi" \
         ~/projects/  \
         $hostname:$dest_projects
 else
@@ -137,8 +147,8 @@ fi
 # Backup images
 if [[ "$dest_images" != "" ]]; then
     print_green "Backing up images to $hostname:$dest_images"
-    rsync -a -r -z -v -u -h $AND_delete --progress --partial --timeout=30 \
-        --exclude="\!pf/*" \
+    rsync $OPT_DRYRUN -a -r -z -v -u -h $OPT_DELETE --progress --partial --timeout=30 \
+        --exclude="- \!pf/" \
         ~/images/  \
         $hostname:$dest_images
 else
@@ -148,8 +158,8 @@ fi
 # Backup pictures
 if [[ "$dest_pictures" != "" ]]; then
     print_green "Backing up pictures to $hostname:$dest_pictures"
-    rsync -a -r -z -v -u -h $AND_delete --progress --partial --timeout=30 \
-        --exclude="iPhoto Library/*" \
+    rsync $OPT_DRYRUN -a -r -z -v -u -h $OPT_DELETE --progress --partial --timeout=30 \
+        --exclude="- iPhoto Library/" \
         ~/Pictures/  \
         $hostname:$dest_pictures
 else
@@ -159,7 +169,7 @@ fi
 # Backup iTunes
 if [[ "$dest_itunes" != "" ]]; then
     print_green "Backing up iTunes to $hostname:$dest_itunes"
-    rsync -r -z -v -u -h --delete --progress --partial --timeout=30 \
+    rsync $OPT_DRYRUN -r -z -v -u -h --delete --progress --partial --timeout=30 \
         ~/Music/iTunes/iTunes\ Media/  \
         $hostname:$dest_itunes
 else
@@ -169,7 +179,7 @@ fi
 # Backup camera
 if [[ "$dest_camera" != "" ]]; then
     print_green "Backing up camera to $hostname:$dest_camera"
-    rsync -a -r -z -v -u -h --progress --partial --timeout=30 \
+    rsync $OPT_DRYRUN -a -r -z -v -u -h --progress --partial --timeout=30 \
         ~/camera/  \
         $hostname:$dest_camera
 else
@@ -177,9 +187,9 @@ else
 fi
 
 # Backup porn
-if [[ "$dest_porn" != "" ]]; then
+if [[ "$dest_porn" != "" && -d ~/porn ]]; then
     print_green "Backing up porn to $hostname:$dest_porn"
-    rsync -a -r -z -v -u -h --delete --progress --partial --timeout=30 \
+    rsync $OPT_DRYRUN -a -r -z -v -u -h --delete --progress --partial --timeout=30 \
         ~/porn/  \
         $hostname:$dest_porn
 else
@@ -189,10 +199,22 @@ fi
 # Backup mirrors
 if [[ "$dest_mirror" != "" ]]; then
     print_green "Backing up mirrors to $hostname:$dest_mirror"
-    rsync -a -r -z -v -u -h --delete --progress --partial --timeout=30 \
+    rsync $OPT_DRYRUN -a -r -z -v -u -h --delete --progress --partial --timeout=30 \
+        --exclude="- 4ch/" \
+        --exclude="- rss/" \
         ~/Downloads/mirror/  \
-        --exclude="4ch/*" \
         $hostname:$dest_mirror
+
+    if [[ -d ~/Downloads/mirror/4ch ]]; then
+        # Only sync 4ch if it exists
+        print_green "Synching 4ch folder"
+        rsync $OPT_DRYRUN -a -r -z -v -u -h --delete --progress --partial --timeout=30 --delete \
+            ~/Downloads/mirror/4ch/  \
+            $hostname:"$dest_mirror/4ch/"
+    else
+        print_yellow "Not synching 4ch folder"
+    fi
+
 else
     print_yellow "No destination for mirrors"
 fi
